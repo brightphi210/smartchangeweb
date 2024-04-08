60df2a38a8b5445f84d12250fc19df5d15497acf
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-04-08 09:59:19 -0700

    a

--- src/Pages/TransactionHistory.js
+++ src/Pages/TransactionHistory.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from '../Components/SideBar'
 import Navbar from '../Components/Navbar'
 import TransactionHistoryCompo from '../Components/TransactionHistoryCompo'
 
 const TransactionHistory = () => {
-  return (
-    <div className='flex w-full'>
-        <SideBar />
 
-        <div className='w-full pl-72'>
-            <Navbar />
+  const [show, setShow] = useState(false)
+
+  const handleShow = () =>{
+    setShow(true)
+  }
+
+  const handleHide = () =>{
+    setShow(false)
+  }
+
+
+  return (
+    <div className='flex w-full relative'>
+      <div  className={`${show === false ? 'hidden' : 'block'} lg:relative lg:w-0 fixed w-10/12 bg-yellow-50 h-full  left-0 z-50`}><SideBar onClick={handleHide}/></div>
+
+      <div className='hidden lg:block'><SideBar /></div>
+
+        <div className='w-full lg:pl-72 pl-0'>
+            <Navbar onClick={handleShow}/>
             <TransactionHistoryCompo />
         </div>
     </div>
