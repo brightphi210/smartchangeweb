3bcad486419884f7fee4e145227698551d2bb4f7
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-04-08 10:25:33 -0700

    a

--- src/Pages/DashboardOverviewMerchant.js
+++ src/Pages/DashboardOverviewMerchant.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from '../Components/SideBar'
 import Navbar from '../Components/Navbar'
 import DashOverviewMerchantCompo from '../Components/DashOverviewMerchantCompo'
 
 const DashboardOverviewMerchant = () => {
+
+  const [show, setShow] = useState(false)
+
+  const handleShow = () =>{
+    setShow(true)
+  }
+
+  const handleHide = () =>{
+    setShow(false)
+  }
+
+
   return (
     <div className='flex w-full'>
-        <SideBar />
+      <div  className={`${show === false ? 'hidden' : 'block'} lg:relative lg:w-0 fixed w-10/12 bg-yellow-50 h-full  left-0 z-50`}><SideBar onClick={handleHide}/></div>
+
+        <div className='hidden lg:block'><SideBar /></div>
         <div className='w-full pl-72'>
-            <Navbar />
+            <Navbar onClick={handleShow}/>
             <DashOverviewMerchantCompo />
         </div>
     </div>
--- src/Pages/DashboardOverviewUser.js
+++ src/Pages/DashboardOverviewUser.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from '../Components/SideBar'
 import Navbar from '../Components/Navbar'
 import DashOverviewUserCompo from '../Components/DashOverviewUserCompo'
 
 const DashboardOverviewUser = () => {
+
+  const [show, setShow] = useState(false)
+
+  const handleShow = () =>{
+    setShow(true)
+  }
+
+  const handleHide = () =>{
+    setShow(false)
+  }
   return (
     <div className='flex w-full'>
-        <SideBar />
-        <div className='w-full pl-72'>
-          <Navbar />
+        <div  className={`${show === false ? 'hidden' : 'block'} lg:relative lg:w-0 fixed w-10/12 bg-yellow-50 h-full  left-0 z-50`}><SideBar onClick={handleHide}/></div>
+        <div className='hidden lg:block'><SideBar /></div>
+        <div className='w-full lg:pl-72 pl-0'>
+          <Navbar onClick={handleShow}/>
           <DashOverviewUserCompo />
         </div>
     </div>
