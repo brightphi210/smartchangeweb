3f51beb0c4cc385fd509ff4e7694db96e93c9adc
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-04-08 09:43:15 -0700

    a

--- src/Pages/DashboardHome.js
+++ src/Pages/DashboardHome.js
@@ -16,7 +16,7 @@ const DashboardHome = () => {
   }
   return (
     <div className='flex w-full relative'>
-        <div  className={`${show === false ? 'hidden' : 'block'} lg:relative lg:w-0 fixed w-10/12 bg-zinc-100 h-full  left-0 z-50`}><SideBar onClick={handleHide}/></div>
+        <div  className={`${show === false ? 'hidden' : 'block'} lg:relative lg:w-0 fixed w-10/12 bg-yellow-100 h-full  left-0 z-50`}><SideBar onClick={handleHide}/></div>
 
         <div className='hidden lg:block'><SideBar /></div>
 
